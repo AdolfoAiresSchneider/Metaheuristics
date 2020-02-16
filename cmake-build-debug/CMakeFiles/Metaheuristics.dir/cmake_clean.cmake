a8ad1be6c87fcb42b46b72dab97876e2d0524d6c
Adolfo Schneider <adolfo.schneider@ifood.com.br>
2020-02-16 12:48:51 -0300

    Recreating the project

---
+++ cmake-build-debug/CMakeFiles/Metaheuristics.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Metaheuristics.dir/main.cpp.o"
+  "Metaheuristics"
+  "Metaheuristics.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Metaheuristics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Metaheuristics.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Metaheuristics.
+# This may be replaced when dependencies are built.
